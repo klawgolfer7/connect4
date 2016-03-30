baf7fd356f7e5b192a5ac13be7284e544674b47e
klawgolfer7 <kevinrlawler@gmail.com>
2016-03-30 00:46:27 -0400

    start of Jquery and minor mods in HTML/CSS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 // Jquery and Javascript for Connect4
 
-$ (document).ready(function() {
+$(document).ready(function() {
 
-	
+	var state = [[0,0,0,0,0,0,0],
+				 [0,0,0,0,0,0,0],
+				 [0,0,0,0,0,0,0],
+				 [0,0,0,0,0,0,0],
+				 [0,0,0,0,0,0,0],
+				 [0,0,0,0,0,0,0]];
+
+
 
 
 });
