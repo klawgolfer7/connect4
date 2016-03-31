f73c519328a46e9dc9564323e4d4cdbfc1bcc216
Kevin Lawler <kevinrlawler@gmail.com>
2016-03-30 21:12:31 -0400

    jquery updated but not finished.  HTML changed to reflect jquery.  player colors added to css

--- js/main.js
+++ js/main.js
@@ -2,14 +2,32 @@
 
 $(document).ready(function() {
 
-	var state = [[0,0,0,0,0,0,0],
-				 [0,0,0,0,0,0,0],
-				 [0,0,0,0,0,0,0],
-				 [0,0,0,0,0,0,0],
-				 [0,0,0,0,0,0,0],
-				 [0,0,0,0,0,0,0]];
+	var currentPlayer = ('player1');
+
+	var state = [[],
+				 [],
+				 [],
+				 [],
+				 [],
+				 [],
+				 []];
+
+		$(".col-1").on('click', function(e) {
+			var $col = $(e.currentTarget);
+			var colindex = $col.index();
+
+			state[colindex].push(currentPlayer);
+
+			var colarray= state[colindex];
+
+			for (var i=0; i<colarray.length; i++);
+				var $cell= $col.find('.cell').get(5-i);
+				$cell.addClass(currentPlayer)
 
 
+
+
+    });
 
 
 });
